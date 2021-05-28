f1d0435f6497567b5b5eb1f1e31c7a138459e741
UnnamedOrange <lycheng1215@sina.com>
2021-05-28 16:53:21 +0800

    修订参数。实现同步器和分频器。

--- ip/PmodAmp3_v1_0/src/PmodAMP3.v
+++ ip/PmodAmp3_v1_0/src/PmodAMP3.v
@@ -11,25 +11,66 @@
 module PmodAMP3 #
 (
 	parameter sample_rate = 44100,
-	parameter MCLK_ratio = 384,
 	parameter resolution = 8,
 	parameter is_stereo = 0,
+	parameter MCLK_ratio = resolution == 24 ? 384 : 256,
 	parameter MCLK_freq = sample_rate * MCLK_ratio,
+	parameter BCLK_freq = sample_rate * resolution * 2,
+	parameter MCLK_divided_by_BCLK = MCLK_freq / BCLK_freq,
+	parameter width = resolution * (1 + is_stereo),
 
+	localparam log_MCLK_divided_by_BCLK =
+		MCLK_divided_by_BCLK == 1 ? 0 :
+		MCLK_divided_by_BCLK == 2 ? 1 :
+		MCLK_divided_by_BCLK == 4 ? 2 :
+		MCLK_divided_by_BCLK == 8 ? 3 :
+		MCLK_divided_by_BCLK == 16 ? 4 : 5,
 	localparam __unused = 0
 )
 (
 	output EX_LRCLK,
 	output EX_SDATA,
 	output EX_BCLK,
-	output EX_MCLE,
-	input [resolution * (1 + is_stereo) - 1 : 0] SAMPLE, // 输入的单次采样。如果是双声道，低位是左声道，高位是右声道。
+	output EX_MCLK,
+	input [width - 1 : 0] SAMPLE,
 	input EN,
 	input RESET_L,
-	input CLK, // 频率为 100 MHz 的全局时钟。
-	input MCLK // 频率为 MCLK_freq 的时钟（默认应为 16.9344 MHz）。
+	input CLK // 不是 100 MHz 时钟。
 );
 
+	reg [width - 1 : 0] sync_sample_buffer, sync_sample; // 同步器。
+	reg sync_en_buffer, sync_en; // 同步器。
+	reg [log_MCLK_divided_by_BCLK : 0] divide_to_bclk; // 分频器。
 
+	reg [width - 1 : 0] to_play; // 保存的值。
+
+	assign EX_MCLK = CLK;
+	assign EX_BCLK = divide_to_bclk[log_MCLK_divided_by_BCLK];
+
+	// 同步器。
+	always @(posedge CLK) begin
+		if (!RESET_L) begin
+			sync_sample_buffer <= 0;
+			sync_sample <= 0;
+			sync_en_buffer <= 0;
+			sync_en <= 0;
+		end
+		else begin
+			sync_sample_buffer <= SAMPLE;
+			sync_sample <= sync_sample_buffer;
+			sync_en_buffer <= EN;
+			sync_en <= sync_en_buffer;
+		end
+	end
+
+	// 分频器，本质是一个行波计数器。
+	always @(posedge CLK) begin
+		if (!RESET_L) begin
+			divide_to_bclk <= 0;
+		end
+		else begin
+			divide_to_bclk <= divide_to_bclk + 1;
+		end
+	end
 
 endmodule
--- ip/PmodAmp3_v1_0/xgui/PmodAMP3_v1_0.tcl
+++ ip/PmodAmp3_v1_0/xgui/PmodAMP3_v1_0.tcl
@@ -11,11 +11,47 @@ proc init_gui { IPINST } {
   set_property tooltip {采样分辨率，单位为比特。} ${resolution}
   set is_stereo [ipgui::add_param $IPINST -name "is_stereo"]
   set_property tooltip {是否使用双声道，如果使用，输入的采样数据位宽翻倍，高位是右声道。} ${is_stereo}
-  set MCLK_ratio [ipgui::add_param $IPINST -name "MCLK_ratio" -widget comboBox]
-  set_property tooltip {外部时钟倍率。外部时钟的频率为采样率乘以倍率。} ${MCLK_ratio}
+  set MCLK_ratio [ipgui::add_param $IPINST -name "MCLK_ratio"]
+  set_property tooltip {外部主时钟倍率。外部主时钟的频率为采样率乘以倍率。} ${MCLK_ratio}
   set MCLK_freq [ipgui::add_param $IPINST -name "MCLK_freq"]
   set_property tooltip {外部时钟频率。} ${MCLK_freq}
+  set BCLK_freq [ipgui::add_param $IPINST -name "BCLK_freq"]
+  set_property tooltip {BCLK 时钟频率。} ${BCLK_freq}
+  set MCLK_divided_by_BCLK [ipgui::add_param $IPINST -name "MCLK_divided_by_BCLK"]
+  set_property tooltip {BCLK 分频数。} ${MCLK_divided_by_BCLK}
 
+}
+
+proc update_PARAM_VALUE.BCLK_freq { PARAM_VALUE.BCLK_freq PARAM_VALUE.sample_rate PARAM_VALUE.resolution } {
+	# Procedure called to update BCLK_freq when any of the dependent parameters in the arguments change
+	
+	set BCLK_freq ${PARAM_VALUE.BCLK_freq}
+	set sample_rate ${PARAM_VALUE.sample_rate}
+	set resolution ${PARAM_VALUE.resolution}
+	set values(sample_rate) [get_property value $sample_rate]
+	set values(resolution) [get_property value $resolution]
+	set_property value [gen_USERPARAMETER_BCLK_freq_VALUE $values(sample_rate) $values(resolution)] $BCLK_freq
+}
+
+proc validate_PARAM_VALUE.BCLK_freq { PARAM_VALUE.BCLK_freq } {
+	# Procedure called to validate BCLK_freq
+	return true
+}
+
+proc update_PARAM_VALUE.MCLK_divided_by_BCLK { PARAM_VALUE.MCLK_divided_by_BCLK PARAM_VALUE.MCLK_freq PARAM_VALUE.BCLK_freq } {
+	# Procedure called to update MCLK_divided_by_BCLK when any of the dependent parameters in the arguments change
+	
+	set MCLK_divided_by_BCLK ${PARAM_VALUE.MCLK_divided_by_BCLK}
+	set MCLK_freq ${PARAM_VALUE.MCLK_freq}
+	set BCLK_freq ${PARAM_VALUE.BCLK_freq}
+	set values(MCLK_freq) [get_property value $MCLK_freq]
+	set values(BCLK_freq) [get_property value $BCLK_freq]
+	set_property value [gen_USERPARAMETER_MCLK_divided_by_BCLK_VALUE $values(MCLK_freq) $values(BCLK_freq)] $MCLK_divided_by_BCLK
+}
+
+proc validate_PARAM_VALUE.MCLK_divided_by_BCLK { PARAM_VALUE.MCLK_divided_by_BCLK } {
+	# Procedure called to validate MCLK_divided_by_BCLK
+	return true
 }
 
 proc update_PARAM_VALUE.MCLK_freq { PARAM_VALUE.MCLK_freq PARAM_VALUE.sample_rate PARAM_VALUE.MCLK_ratio } {
@@ -34,8 +70,13 @@ proc validate_PARAM_VALUE.MCLK_freq { PARAM_VALUE.MCLK_freq } {
 	return true
 }
 
-proc update_PARAM_VALUE.MCLK_ratio { PARAM_VALUE.MCLK_ratio } {
+proc update_PARAM_VALUE.MCLK_ratio { PARAM_VALUE.MCLK_ratio PARAM_VALUE.resolution } {
 	# Procedure called to update MCLK_ratio when any of the dependent parameters in the arguments change
+	
+	set MCLK_ratio ${PARAM_VALUE.MCLK_ratio}
+	set resolution ${PARAM_VALUE.resolution}
+	set values(resolution) [get_property value $resolution]
+	set_property value [gen_USERPARAMETER_MCLK_ratio_VALUE $values(resolution)] $MCLK_ratio
 }
 
 proc validate_PARAM_VALUE.MCLK_ratio { PARAM_VALUE.MCLK_ratio } {
@@ -70,6 +111,15 @@ proc validate_PARAM_VALUE.sample_rate { PARAM_VALUE.sample_rate } {
 	return true
 }
 
+proc update_PARAM_VALUE.width { PARAM_VALUE.width } {
+	# Procedure called to update width when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.width { PARAM_VALUE.width } {
+	# Procedure called to validate width
+	return true
+}
+
 
 proc update_MODELPARAM_VALUE.sample_rate { MODELPARAM_VALUE.sample_rate PARAM_VALUE.sample_rate } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
@@ -94,5 +144,20 @@ proc update_MODELPARAM_VALUE.MCLK_ratio { MODELPARAM_VALUE.MCLK_ratio PARAM_VALU
 proc update_MODELPARAM_VALUE.MCLK_freq { MODELPARAM_VALUE.MCLK_freq PARAM_VALUE.MCLK_freq } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.MCLK_freq}] ${MODELPARAM_VALUE.MCLK_freq}
+}
+
+proc update_MODELPARAM_VALUE.width { MODELPARAM_VALUE.width PARAM_VALUE.width } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.width}] ${MODELPARAM_VALUE.width}
+}
+
+proc update_MODELPARAM_VALUE.BCLK_freq { MODELPARAM_VALUE.BCLK_freq PARAM_VALUE.BCLK_freq } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.BCLK_freq}] ${MODELPARAM_VALUE.BCLK_freq}
+}
+
+proc update_MODELPARAM_VALUE.MCLK_divided_by_BCLK { MODELPARAM_VALUE.MCLK_divided_by_BCLK PARAM_VALUE.MCLK_divided_by_BCLK } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.MCLK_divided_by_BCLK}] ${MODELPARAM_VALUE.MCLK_divided_by_BCLK}
 }
 
