d4f8204838a334f8cbf8c9b730a1e0bfc1e2d432
UnnamedOrange <lycheng1215@sina.com>
2021-06-05 21:38:12 +0800

    修复 FIFO 控制器个性化页面的不正确显示。

--- ip/fifo_controller/xgui/fifo_controller_v1_0.tcl
+++ ip/fifo_controller/xgui/fifo_controller_v1_0.tcl
@@ -1,9 +1,6 @@
 # Definitional proc to organize widgets for parameters.
 proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
-  #Adding Page
-  ipgui::add_page $IPINST -name "Page 0"
-
   set data_width [ipgui::add_param $IPINST -name "data_width" -widget comboBox]
   set_property tooltip {Êý¾ÝÎ»¿í¡£} ${data_width}
 
