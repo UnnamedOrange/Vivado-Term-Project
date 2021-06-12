798958778931af5defb778e884e696e9933bb0f9
UnnamedOrange <lycheng1215@sina.com>
2021-06-12 17:01:03 +0800

    实现基地址的初始化。

--- ip/core/src/core.v
+++ ip/core/src/core.v
@@ -29,41 +29,41 @@ module core_t #
 	output [12:0] db_addr_w,
 	output [23:0] db_data_in,
 	output db_en_w,
-	output [12:0] db_addr_r,
+	output reg [12:0] db_addr_r,
 	input [23:0] db_data_out,
-	output db_en_r,
+	output reg db_en_r,
 
 	// .object (do) 对应 BRAM。
 	output [11:0] do_addr_w,
 	output [7:0] do_data_in,
 	output do_en_w,
-	output [12:0] do_addr_r,
+	output reg [12:0] do_addr_r,
 	input [3:0] do_data_out,
-	output do_en_r,
+	output reg do_en_r,
 
 	// .pixel (dp) 对应 BRAM。
 	output [12:0] dp_addr_w,
 	output [31:0] dp_data_in,
 	output dp_en_w,
-	output [12:0] dp_addr_r,
+	output reg [12:0] dp_addr_r,
 	input [31:0] dp_data_out,
-	output dp_en_r,
+	output reg dp_en_r,
 
 	// .timing (dt) 对应 BRAM。
 	output [11:0] dt_addr_w,
 	output [31:0] dt_data_in,
 	output dt_en_w,
-	output [11:0] dt_addr_r,
+	output reg [11:0] dt_addr_r,
 	input [31:0] dt_data_out,
-	output dt_en_r,
+	output reg dt_en_r,
 
 	// .skin (ds) 对应 BRAM。
 	output [14:0] ds_addr_w,
 	output [15:0] ds_data_in,
 	output ds_en_w,
-	output [14:0] ds_addr_r,
+	output reg [14:0] ds_addr_r,
 	input [15:0] ds_data_out,
-	output ds_en_r,
+	output reg ds_en_r,
 
 	// 选歌开关。
 	input [7:0] song_selection, // 假设不变。
@@ -122,11 +122,30 @@ module core_t #
 		s_w_load_beatmap_3  = 16'h0010, // 等待加载 .timing 到 BRAM。
 		s_reset_cpu_song    = 16'h0011, // 重置 CPU 到歌曲播放。
 		s_w_reset_cpu_song  = 16'h0012, // 等待重置 CPU 到歌曲播放。
+		s_get_base_addr_0   = 16'h0013, // 获取 .beatmap 的各个基地址。
+		s_w_get_base_addr_0 = 16'h0014, // 等待获取 .beatmap 的各个基地址和数量。
+		s_get_base_addr_1   = 16'h0015, // 获取 .object 的各个基地址和数量。
+		s_w_get_base_addr_1 = 16'h0016, // 等待获取 .object 的各个基地址和数量。
+		s_get_base_addr_2   = 16'h0017, // 获取 .pixel 的各个基地址和数量。
+		s_w_get_base_addr_2 = 16'h0018, // 等待获取 .pixel 的各个基地址和数量。
+		s_get_base_addr_3   = 16'h0019, // 获取 .timing 的各个基地址和数量。
+		s_w_get_base_addr_3 = 16'h0020, // 等待获取 .timing 的各个基地址和数量。
 		s_standby           = 16'h9999, // 游戏结束，不做其他事（暂时保留）。
 		s_unused = 16'hffff;
 	reg [state_width - 1 : 0] state, n_state;
 
-	/* 模块互联。*/
+	/* 关键变量。*/
+	reg [12:0] db_size[0:3];
+	reg [12:0] db_base_addr[0:3];
+	reg [12:0] do_size[0:3];
+	reg [12:0] do_base_addr[0:3];
+	reg [12:0] dp_size[0:3]; // == db_size
+	reg [12:0] dp_base_addr[0:3];
+	reg [11:0] dt_size;
+	reg [11:0] dt_base_addr; // == 2
+	reg [19:0] song_length;
+
+	/* 模块互联与信号量。*/
 	// BRAM。
 	wire sig_db_on;
 	wire sig_db_done;
@@ -342,6 +361,207 @@ module core_t #
 		.CLK(CLK)
 	);
 
+	// get_base_addr。
+	wire sig_get_base_addr_0_on;
+	reg sig_get_base_addr_0_done;
+	wire sig_get_base_addr_1_on;
+	reg sig_get_base_addr_1_done;
+	wire sig_get_base_addr_2_on;
+	reg sig_get_base_addr_2_done;
+	wire sig_get_base_addr_3_on;
+	reg sig_get_base_addr_3_done;
+
+	always @(posedge CLK) begin: get_base_addr_0_t
+		integer i;
+		reg [2:0] which;
+		reg [1:0] pat;
+
+		if (!RESET_L) begin
+			for (i = 0; i < 4; i = i + 1)
+				db_size[i] <= 0;
+			for (i = 0; i < 4; i = i + 1)
+				db_base_addr[i] <= 0;
+			sig_get_base_addr_0_done <= 0;
+			which <= 0;
+			pat <= 0;
+			db_en_r <= 0;
+			db_addr_r <= 0;
+		end
+		else begin
+			if (!which[2]) begin
+				if (sig_get_base_addr_0_on)
+					which[2] <= 1;
+			end
+			else begin
+				if (pat == 0) begin
+					if (which[1:0] == 2'd0)
+						db_addr_r <= 0;
+					else
+						db_addr_r <= db_base_addr[which[1:0] - 1] + db_size[which[1:0] - 1];
+					db_en_r <= 1;
+				end
+				else if (pat == 3) begin // 改成 == 2 也可，但是需要额外处理 pat。这里利用了自然溢出。
+					db_size[which[1:0]] <= db_data_out;
+					if (which[1:0] == 2'd0)
+						db_base_addr[0] <= 1;
+					else
+						db_base_addr[which[1:0]] <= db_base_addr[which[1:0] - 1] + db_size[which[1:0] - 1] + 1;
+					db_en_r <= 0;
+					if (which[1:0] < 3)
+						which[1:0] <= which[1:0] + 1;
+					else
+						which <= 3'b0;
+				end
+				pat <= pat + 1;
+			end
+			sig_get_base_addr_0_done <= which == 3'b111 && pat == 2'b11;
+		end
+	end
+
+	always @(posedge CLK) begin: get_base_addr_1_t
+		integer i;
+		reg [2:0] which;
+		reg [1:0] part;
+		reg [1:0] pat;
+
+		if (!RESET_L) begin
+			for (i = 0; i < 4; i = i + 1)
+				do_size[i] <= 0;
+			for (i = 0; i < 4; i = i + 1)
+				do_base_addr[i] <= 0;
+			sig_get_base_addr_1_done <= 0;
+			which <= 0;
+			part <= 0;
+			pat <= 0;
+			do_en_r <= 0;
+			do_addr_r <= 0;
+		end
+		else begin
+			if (!which[2]) begin
+				if (sig_get_base_addr_1_on)
+					which[2] <= 1;
+			end
+			else begin
+				if (pat == 0) begin
+					if (which[1:0] == 2'd0)
+						do_addr_r <= part;
+					else
+						do_addr_r <= do_base_addr[which[1:0] - 1] + do_size[which[1:0] - 1] + part;
+					do_en_r <= 1;
+				end
+				else if (pat == 3) begin
+					do_size[which[1:0]][part * 4 +: 4] <= do_data_out;
+					part <= part + 1;
+					do_en_r <= 0;
+					if (part == 2'b11) begin
+						if (which[1:0] == 2'd0)
+							do_base_addr[0] <= 4;
+						else
+							do_base_addr[which[1:0]] <= do_base_addr[which[1:0] - 1] + do_size[which[1:0] - 1] + 4;
+
+						if (which[1:0] < 3)
+							which[1:0] <= which[1:0] + 1;
+						else
+							which <= 3'b0;
+					end
+				end
+				pat <= pat + 1;
+			end
+			sig_get_base_addr_1_done <= which == 3'b111 && part == 2'b11 && pat == 2'b11;
+		end
+	end
+
+	always @(posedge CLK) begin: get_base_addr_2_t
+		integer i;
+		reg [2:0] which;
+		reg [1:0] pat;
+
+		if (!RESET_L) begin
+			for (i = 0; i < 4; i = i + 1)
+				dp_size[i] <= 0;
+			for (i = 0; i < 4; i = i + 1)
+				dp_base_addr[i] <= 0;
+			sig_get_base_addr_2_done <= 0;
+			which <= 0;
+			pat <= 0;
+			dp_en_r <= 0;
+			dp_addr_r <= 0;
+		end
+		else begin
+			if (!which[2]) begin
+				if (sig_get_base_addr_2_on)
+					which[2] <= 1;
+			end
+			else begin
+				if (pat == 0) begin
+					if (which[1:0] == 2'd0)
+						dp_addr_r <= 0;
+					else
+						dp_addr_r <= dp_base_addr[which[1:0] - 1] + dp_size[which[1:0] - 1];
+					dp_en_r <= 1;
+				end
+				else if (pat == 3) begin
+					dp_size[which[1:0]] <= dp_data_out;
+					if (which[1:0] == 2'd0)
+						dp_base_addr[0] <= 1;
+					else
+						dp_base_addr[which[1:0]] <= dp_base_addr[which[1:0] - 1] + dp_size[which[1:0] - 1] + 1;
+					dp_en_r <= 0;
+					if (which[1:0] < 3)
+						which[1:0] <= which[1:0] + 1;
+					else
+						which <= 3'b0;
+				end
+				pat <= pat + 1;
+			end
+			sig_get_base_addr_2_done <= which == 3'b111 && pat == 2'b11;
+		end
+	end
+
+	always @(posedge CLK) begin: get_base_addr_3_t
+		integer i;
+		reg [1:0] which;
+		reg [1:0] pat;
+
+		if (!RESET_L) begin
+			dt_size <= 0;
+			dt_base_addr <= 2; // 总是 2。
+			sig_get_base_addr_3_done <= 0;
+			which <= 0;
+			pat <= 0;
+			dt_en_r <= 0;
+			dt_addr_r <= 0;
+		end
+		else begin
+			if (!which[1]) begin
+				if (sig_get_base_addr_3_on)
+					which[1] <= 1;
+			end
+			else begin
+				if (pat == 0) begin
+					if (which[0:0] == 1)
+						dt_addr_r <= 1;
+					else
+						dt_addr_r <= 0;
+					dt_en_r <= 1;
+				end
+				else if (pat == 3) begin
+					if (which[0:0] == 1)
+						song_length <= dt_data_out;
+					else
+						dt_size <= dt_data_out;
+					dt_en_r <= 0;
+					if (which[0:0] < 1)
+						which[0:0] <= which[0:0] + 1;
+					else
+						which <= 2'b0;
+				end
+				pat <= pat + 1;
+			end
+			sig_get_base_addr_3_done <= which == 2'b11 && pat == 2'b11;
+		end
+	end
+
 	/* 调试输出。*/
 	assign DEBUG_CURRENT_STATE = state;
 
@@ -385,7 +605,23 @@ module core_t #
 			s_reset_cpu_song:
 				n_state = s_w_reset_cpu_song;
 			s_w_reset_cpu_song:
-				n_state = sig_reset_cpu_song_done ? s_standby : s_w_reset_cpu_song;
+				n_state = sig_reset_cpu_song_done ? s_get_base_addr_0 : s_w_reset_cpu_song;
+			s_get_base_addr_0:
+				n_state = s_w_get_base_addr_0;
+			s_w_get_base_addr_0:
+				n_state = sig_get_base_addr_0_done ? s_get_base_addr_1 : s_w_get_base_addr_0;
+			s_get_base_addr_1:
+				n_state = s_w_get_base_addr_1;
+			s_w_get_base_addr_1:
+				n_state = sig_get_base_addr_1_done ? s_get_base_addr_2 : s_w_get_base_addr_1;
+			s_get_base_addr_2:
+				n_state = s_w_get_base_addr_2;
+			s_w_get_base_addr_2:
+				n_state = sig_get_base_addr_2_done ? s_get_base_addr_3 : s_w_get_base_addr_2;
+			s_get_base_addr_3:
+				n_state = s_w_get_base_addr_3;
+			s_w_get_base_addr_3:
+				n_state = sig_get_base_addr_3_done ? s_standby : s_w_get_base_addr_3;
 			s_standby:
 				n_state = s_standby;
 			default:
@@ -403,5 +639,11 @@ module core_t #
 
 	// reset_cpu_song。
 	assign sig_reset_cpu_song_on = state == s_reset_cpu_song;
+
+	// get_base_addr。
+	assign sig_get_base_addr_0_on = state == s_get_base_addr_0;
+	assign sig_get_base_addr_1_on = state == s_get_base_addr_1;
+	assign sig_get_base_addr_2_on = state == s_get_base_addr_2;
+	assign sig_get_base_addr_3_on = state == s_get_base_addr_3;
 
 endmodule
