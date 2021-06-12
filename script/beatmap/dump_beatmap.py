d5e16bf2fd4c1badcd445ef7e9b1c34e15713056
UnnamedOrange <lycheng1215@sina.com>
2021-06-12 14:25:48 +0800

    编写生成 .object 的脚本。修复 .timing 文件移位错误的问题。

--- script/beatmap/dump_beatmap.py
+++ script/beatmap/dump_beatmap.py
@@ -3,7 +3,10 @@ import librosa
 
 g_basic_timing = 109  # 基础速度。
 g_timing = []  # [时间点, 速度]
-g_timing_final = []
+g_timing_final = None
+g_original_object = [[] for _ in range(4)]
+g_object = [[] for _ in range(4)]
+g_object_final = None
 
 if __name__ == '__main__':
     sound, sample_rate = librosa.load('audio.mp3', 44100)
@@ -59,20 +62,70 @@ if __name__ == '__main__':
     g_timing = final_timing
     del final_timing
 
-    print('报告：时间点占用 %d / 4094 个' % len(g_timing))
+    print('报告：关键时间点占用 %d / 4094 个' % len(g_timing))
     if len(g_timing) > 4094:
-        print('错误：时间点超过数量限制')
+        print('错误：关键时间点超过数量限制')
         exit()
 
     # 构建 timing。
+    g_timing_final = []
     g_timing_final.append(len(g_timing))
     g_timing_final.append(length_in_milli)
     for i in range(len(g_timing)):
-        g_timing_final.append((g_timing[i][1] << 24) + g_timing[i][0])
+        g_timing_final.append((g_timing[i][1] << 20) + g_timing[i][0])
     g_timing_final = b''.join(x.to_bytes(
         4, byteorder='little', signed=False) for x in g_timing_final)
     print('报告：.timing 文件大小为 %d 字节' % len(g_timing_final))
 
+    # 解析 osu 谱面文件。
+    for i in range(len(lines)):
+        if (lines[i] == '[HitObjects]'):
+            break
+    i += 1
+
+    original_object = []
+    while lines[i] != '':
+        original_object.append(lines[i])
+        i += 1
+    original_object = [x.split(',') for x in original_object]
+
+    for i in range(len(original_object)):
+        idx = int(original_object[i][0]) * 4 // 512
+        st = round(float(original_object[i][2]))
+        is_hit = original_object[i][3] == '1'
+        et = round(float(original_object[i][5].split(':')[0]))
+        g_original_object[idx].append([st, et])  # [起始, 结束（没有就是空）]
+
+    print('报告：对象占用 %d / 8176 个' % sum(len(x) for x in g_original_object))
+    if sum(len(x) for x in g_original_object) > 8176:
+        print('错误：对象超过数量限制')
+        exit()
+
+    print('报告：时间点占用 %d / 8188 个' %
+          sum(1 + bool(x[1]) for y in g_original_object for x in y))
+    if sum(1 + bool(x[1]) for y in g_original_object for x in y) > 8188:
+        print('错误：时间点超过数量限制')
+        exit()
+
+    # 构建 object。
+    g_object = []
+    for i in range(4):
+        g_object.append(len(g_original_object[i]) & 15)
+        g_object.append((len(g_original_object[i]) >> 4) & 15)
+        g_object.append((len(g_original_object[i]) >> 8) & 15)
+        g_object.append((len(g_original_object[i]) >> 12) & 15)
+        for j in range(len(g_original_object[i])):
+            g_object.append(bool(g_original_object[i][j][1]))
+    g_object_final = [((g_object[i * 2 + 1] << 4) + g_object[i * 2]).to_bytes(
+        1, byteorder='little', signed=False) for i in range(len(g_object) // 2)]
+    if len(g_object) % 2:
+        g_object_final.append(g_object[-1].to_bytes(
+            1, byteorder='little', signed=False))
+    g_object_final = b''.join(g_object_final)
+    print('报告：.object 文件大小为 %d 字节' % len(g_object_final))
+
     # 写文件。
     with open('.timing', 'wb') as file:
         file.write(g_timing_final)
+    with open('.object', 'wb') as file:
+        file.write(g_object_final)
