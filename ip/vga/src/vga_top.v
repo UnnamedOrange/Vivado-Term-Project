4ba3057ae76eb02fbc438919e62e65683ff0a23a
UnnamedOrange <lycheng1215@sina.com>
2021-06-15 19:41:08 +0800

    修复 VGA 复位的有效性与变量名不同的问题。

--- ip/vga/src/vga_top.v
+++ ip/vga/src/vga_top.v
@@ -7,6 +7,7 @@
 /// <version>
 /// 0.0.1 (UnnamedOrange and Jack-Lyu) : First commit.
 /// 0.0.2 (Jack-Lyu) : VGA模块搭建完毕。
+/// 0.0.3 (UnnamedOrange) : 修复复位的有效性与变量名不同的问题。
 /// </version>
 
 `timescale 1ns / 1ps
@@ -30,19 +31,14 @@ module vga_top(
 	wire valid;
 	wire[9:0] h_cnt;
 	wire[9:0] v_cnt;
-	reg rst_n;
-	always @(posedge clk)
-		rst_n <= ~rst;
-	dcm_25m1 u0 (.clk_in(clk),.clk_out(pclk),.reset(rst_n));
-	vga_640x480 u1(.pclk(pclk),.reset(rst_n),.hsync(hsync),.vsync(vsync),.valid(valid),.h_cnt(h_cnt),.v_cnt(v_cnt));
+	dcm_25m1 u0 (.clk_in(clk),.clk_out(pclk),.reset(rst));
+	vga_640x480 u1(.pclk(pclk),.reset(rst),.hsync(hsync),.vsync(vsync),.valid(valid),.h_cnt(h_cnt),.v_cnt(v_cnt));
 	vga_display u2(.vidon(valid),.h_cnt(h_cnt),.v_cnt(v_cnt),.vga_r(vga_r),.vga_b(vga_b),.vga_g(vga_g),
 	.get_r(get_r),.get_g(get_g),.get_b(get_b),
-	.rst(rst_n),.clk_25(pclk)
+	.rst(rst),.clk_25(pclk)
 	);
-	request_controller u3(.clk(clk),.rst(rst_n),.clk_25(pclk),
+	request_controller u3(.clk(clk),.rst(rst),.clk_25(pclk),
 	.h_cnt(h_cnt),.v_cnt(v_cnt),.valid(valid),
 	.request(request),.hcnt_request(hcnt_request),.vcnt_request(vcnt_request));
-
-
 
 endmodule
