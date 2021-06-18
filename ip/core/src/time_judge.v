9f5c4986a991d1f85d1d896272af190dfafa89c2
UnnamedOrange <lycheng1215@sina.com>
2021-06-18 21:30:05 +0800

    修复键盘延时出现负数的问题。
    将参数改为 OD0。

--- ip/core/src/time_judge.v
+++ ip/core/src/time_judge.v
@@ -12,12 +12,12 @@
 
 module time_judge_t #
 (
-	parameter t_perfect = 15,
+	parameter t_perfect = 17,
 	parameter t_great = 50,
-	parameter t_good = 115,
-	parameter t_bad = 150,
+	parameter t_good = 83,
+	parameter t_bad = 113,
 	parameter t_miss = 200,
-	parameter t_offset = 0 // 键盘输入延迟修正。
+	parameter t_offset = 8 // 键盘输入延迟修正。
 )
 (
 	input [19:0] current_time,
@@ -32,8 +32,9 @@ module time_judge_t #
 	output reg in_miss // 只有早到 miss，没有晚到 miss。
 );
 
-	wire [19:0] true_time;
-	assign true_time = current_time - t_offset;
+	wire [19:0] true_time, true_object_time;
+	assign true_time = current_time;
+	assign true_object_time = object_time + t_offset;
 
 	always @(*) begin
 		too_early = 0;
@@ -44,32 +45,32 @@ module time_judge_t #
 		in_bad = 0;
 		in_miss = 0;
 
-		if (true_time == object_time)
+		if (true_time == true_object_time)
 			in_perfect = 1;
-		else if (true_time < object_time) begin // 物体在判定线上面。
-			if (object_time - true_time <= t_perfect)
+		else if (true_time < true_object_time) begin // 物体在判定线上面。
+			if (true_object_time - true_time <= t_perfect)
 				in_perfect = 1;
-			else if (object_time - true_time <= t_great)
+			else if (true_object_time - true_time <= t_great)
 				in_great = 1;
-			else if (object_time - true_time <= t_good)
+			else if (true_object_time - true_time <= t_good)
 				in_good = 1;
-			else if (object_time - true_time <= t_bad)
+			else if (true_object_time - true_time <= t_bad)
 				in_bad = 1;
-			else if (object_time - true_time <= t_miss)
+			else if (true_object_time - true_time <= t_miss)
 				in_miss = 1;
 			else
 				too_early = 1;
 		end
 		else begin // 物体在判定线下面。
-			if (true_time - object_time <= t_perfect)
+			if (true_time - true_object_time <= t_perfect)
 				in_perfect = 1;
-			else if (true_time - object_time <= t_great)
+			else if (true_time - true_object_time <= t_great)
 				in_great = 1;
-			else if (true_time - object_time <= t_good)
+			else if (true_time - true_object_time <= t_good)
 				in_good = 1;
-			else if (true_time - object_time <= t_bad)
+			else if (true_time - true_object_time <= t_bad)
 				in_bad = 1;
-			// else if (true_time - object_time <= t_miss)
+			// else if (true_time - true_object_time <= t_miss)
 			// 	in_miss = 1;
 			else
 				too_late = 1;
